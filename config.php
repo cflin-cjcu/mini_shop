695c114eb2ea060f447d07d0509eb9c9582beac3
cflin <cflin@mail.cjcu.edu.tw>
2020-10-27 13:54:16 +0800

    更改設定

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-define('_SHOP_NAME', 'Tad點心鋪');
+define('_SHOP_NAME', '清峰水果店');
 define('_DB_HOST', 'localhost');
 define('_DB_ID', 'root');
 define('_DB_PW', '12345');
@@ -7,5 +7,5 @@ define('_DB_NAME', 'mini_shop');
 define('_UPLOAD_PATH', 'C:/UniServerZ/www/mini_shop/uploads/');
 define('_UPLOAD_URL', 'http://localhost/mini_shop/uploads/');
 
-$admin_array = ['tad'];
+$admin_array = ['cflin'];
 // error_reporting(0);
